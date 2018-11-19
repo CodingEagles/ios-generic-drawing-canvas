6a47f7c04adbbdf4ea96da3db90bf1f889c6522c
Daniel S A B <iamdanielsab@gmail.com>
2018-11-19 12:09:02 -0300

    add drag and drop implementation

--- ios-generic-drawing-canvas/ios-generic-drawing-canvas/ViewController.swift
+++ ios-generic-drawing-canvas/ios-generic-drawing-canvas/ViewController.swift
@@ -18,8 +18,14 @@ class ViewController: UIViewController, ViewControllerDelegate {
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var redButton: UIButton!
     
+    @IBOutlet weak var imagesView: UIView!
+    
+    @IBOutlet weak var penButton: UIButton!
+    @IBOutlet weak var moveButton: UIButton!
     
     var images: [Image] = []
+    
+    var selectedView: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +33,13 @@ class ViewController: UIViewController, ViewControllerDelegate {
         self.canvasView.isMultipleTouchEnabled = true
         let reqImages = NSFetchRequest<Image>(entityName: "Image")
         lbSizeFont.text = "1.0"
+        self.penButtonPressed("")
         DispatchQueue.main.async {
             self.images = CoreDataManager.sharedManager.fetch(reqImages)!
             print(self.images)
             guard let image = self.images.last ?? nil, let picture = image.picture  else { return }
             self.canvasView.drawImage = UIImage(data: picture)
             self.canvasView.backgroundColor = .clear
-            
         }
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -69,7 +75,7 @@ class ViewController: UIViewController, ViewControllerDelegate {
         lbSizeFont.text = "\(canvasView.path.lineWidth)"
     }
     func drawFinish() {
-        CanvasViewHelper().save(that: self.canvasView)
+        CanvasViewHelper().save(that: imagesView)
     }
     
     func eraseDraw() {
@@ -91,17 +97,89 @@ class ViewController: UIViewController, ViewControllerDelegate {
         
         for button in buttons {
             if unselectButtons.contains(button) {
-                button.layer.cornerRadius = 0
                 button.layer.borderWidth = 0
                 button.layer.borderColor = UIColor.clear.cgColor
             } else {
-                button.layer.cornerRadius = 5
                 button.layer.borderWidth = 3
                 button.layer.borderColor = UIColor.lightGray.cgColor
             }
         }
         
     }
-
+    
+    @IBAction func penButtonPressed(_ sender: Any) {
+        penButton.layer.borderWidth = 5
+        penButton.layer.borderColor = UIColor.white.cgColor
+        moveButton.layer.borderWidth = 0
+        
+        canvasView.isUserInteractionEnabled = true
+        
+        selectedView?.layer.borderWidth = 0
+        selectedView?.layer.borderColor = UIColor.clear.cgColor
+        selectedView = nil
+    }
+    
+    @IBAction func moveButtonPressed(_ sender: Any) {
+        penButton.layer.borderWidth = 0
+        moveButton.layer.borderWidth = 5
+        moveButton.layer.borderColor = UIColor.white.cgColor
+        
+        canvasView.isUserInteractionEnabled = false
+    }
+    
+    @IBAction func addViewButtonPressed(_ sender: Any) {
+        let newView = UIView(frame: CGRect(x: 8, y: 16, width: 150, height: 150))
+        
+        let randomHue = CGFloat(arc4random()%256)/256
+        
+        newView.backgroundColor = UIColor(hue: randomHue, saturation: 1, brightness: 1, alpha: 1)
+        
+        imagesView.addSubview(newView)
+    }
+    
+    @IBAction func tapGestureRecognized(_ sender: Any) {
+        let recognizer = sender as! UITapGestureRecognizer
+        
+        let location = recognizer.location(in: imagesView)
+        
+        let tappedView = imagesView.hitTest(location, with: nil)!
+        
+        print(tappedView)
+        
+        if tappedView == imagesView {
+            
+        } else if tappedView == selectedView {
+            selectedView?.layer.borderWidth = 0
+            selectedView?.layer.borderColor = UIColor.clear.cgColor
+            selectedView = nil
+        } else {
+            selectedView?.layer.borderWidth = 0
+            selectedView?.layer.borderColor = UIColor.clear.cgColor
+            
+            tappedView.layer.borderWidth = 10
+            tappedView.layer.borderColor = UIColor.red.cgColor
+            selectedView = tappedView
+        }
+    }
+    
+    @IBAction func panGestureRecognized(_ sender: Any) {
+        let recognizer = sender as! UIPanGestureRecognizer
+        
+        let location = recognizer.location(in: imagesView)
+        
+        var touchedView = imagesView
+        
+        if imagesView.hitTest(location, with: nil)! == selectedView {
+            touchedView = imagesView.hitTest(location, with: nil)!
+        }
+        
+        if recognizer.state == .changed {
+            
+            if touchedView != imagesView {
+                view.bringSubviewToFront(touchedView!)
+                touchedView!.center = CGPoint(x: location.x, y: location.y)
+            }
+        }
+    }
 }
 
